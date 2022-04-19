ede790adf70cb15ed0def5c2681b8e277ecfd0e3
DmtryP <listochekcs@gmail.com>
2022-04-20 00:46:49 +0300

    123

--- js.js
+++ js.js
@@ -1,14 +1,20 @@
-let data = document.getElementById("data");
+let data = document.getElementById("dataArr");
+
 
 function genFields(){
 
     for(let i = 0 ; i <= 21; i++){
         let trData = document.createElement("tr");
-        trData.setAttribute('id', 'dataString');
+        trData.setAttribute('class', 'dataString');
+        trData.setAttribute('id', `${i}`);
         for(let j = 0 ; j <= 17; j++){
-            let tdData = document.createElement("td");
-            tdData.setAttribute('id', 'dataBlock');
-            trData.appendChild(tdData);
+            let divData= document.createElement("div");
+            divData.setAttribute('id', `${j}`);
+            divData.setAttribute('class', 'data');
+            let thData = document.createElement("th");
+            thData.setAttribute('id', 'dataBlock');
+            thData.appendChild(divData);
+            trData.appendChild(thData);
         }
         data.appendChild(trData);
     }
